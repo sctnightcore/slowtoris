e76fd5e6f1371a48ff8f4ef453914fdaeff64fb2
naganori-san <g.duque@protonmail.ch>
2017-10-02 11:18:18 +0200

    Upload to PyPi

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
 from setuptools import setup
 
 setup(
-  name = 'slowtoris',
-  packages = ['slowtoris'],
-  version = '0.1',
-  description = 'A Python implementation of the Slow Loris attack with support for the Tor network and SOCKS5 proxies.',
-  author = 'Gabriel Duque',
-  author_email = 'g.duque@protonmail.ch',
-  url = 'https://github.com/naganori-san/slowtoris',
-  download_url = 'https://github.com/naganori-san/slowtoris/archive/0.1.tar.gz',
-  keywords = ['hacking-tools', 'security', 'dos-tools'],
-  license = 'MIT',
-  classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Education',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.2',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    ],
-  install_requires = [
-      'click',
-      'PySocks'
-      ]
-)
+        name = 'slowtoris',
+        packages = ['slowtoris'],
+        version = '0.1',
+        description = 'A Python implementation of the Slow Loris attack with support for the Tor network and SOCKS5 proxies.',
+        author = 'Gabriel Duque',
+        author_email = 'g.duque@protonmail.ch',
+        url = 'https://github.com/naganori-san/slowtoris',
+        download_url = 'https://github.com/naganori-san/slowtoris/archive/0.1.tar.gz',
+        keywords = ['hacking-tools', 'security', 'dos-tools'],
+        license = 'MIT',
+        classifiers = [
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Education',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.0',
+            'Programming Language :: Python :: 3.1',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3 :: Only',
+            ],
+        install_requires = [
+            'click',
+            'PySocks'
+            ]
+        )
